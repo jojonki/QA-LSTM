e9ec171646e8b6b22455a5ec0f2a9413ebf6a7ac
jojonki <junki.ohmura@gmail.com>
2017-12-20 14:36:32 -0500

    use argparser and add checkpoints and resume

--- train.py
+++ train.py
@@ -3,8 +3,9 @@
     Ming Tan, Cicero dos Santos, Bing Xiang, Bowen Zhou, ICLR 2016
     https://arxiv.org/abs/1511.04108
 '''
-
+import os
 import random
+import argparse
 from tqdm import tqdm
 import numpy as np
 import torch
@@ -12,6 +13,23 @@ from gensim.models.keyedvectors import KeyedVectors
 from utils import load_data, load_data2, load_vocabulary, Config, load_embd_weights
 from utils import make_vector
 from models import QA_LSTM
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', type=int, default=128, help='input batch size')
+parser.add_argument('--start_epoch', type=int, default=0, help='resume epoch count, default=0')
+parser.add_argument('--n_epochs', type=int, default=4, help='input batch size')
+parser.add_argument('--embd_size', type=int, default=300, help='word embedding size')
+parser.add_argument('--hidden_size', type=int, default=141, help='hidden size of one-directional LSTM')
+parser.add_argument('--max_sent_len', type=int, default=200, help='max sentence length')
+parser.add_argument('--margin', type=float, default=0.2, help='margin for loss function')
+parser.add_argument('--use_pickle', type=int, default=0, help='load dataset from pickles')
+parser.add_argument('--test', type=int, default=0, help='1 for test, or for training')
+parser.add_argument('--seed', type=int, default=1111, help='random seed')
+parser.add_argument('--resume', default='./checkpoints/model_best.tar', type=str, metavar='PATH', help='path saved params')
+args = parser.parse_args()
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
 
 PAD = '<PAD>'
 id_to_word, label_to_ans, label_to_ans_text = load_vocabulary('./V2/vocabulary', './V2/InsuranceQA.label2answer.token.encoded')
@@ -25,18 +43,8 @@ test_data = load_data2('./V2/InsuranceQA.question.anslabel.token.500.pool.solr.t
 print('n_train:', len(train_data))
 print('n_test:', len(test_data))
 
-margin       = 0.2
-embd_size    = 300
-hidden_size  = 141
-args = {
-    'max_sent_len': 200,
-    'margin'      : margin,
-    'vocab_size'  : vocab_size,
-    'embd_size'   : embd_size,
-    'hidden_size' : hidden_size,
-    'pre_embd'    : None
-}
-args = Config(**args)
+args.vocab_size = vocab_size
+args.pre_embd   = None
 
 print('loading a word2vec binary...')
 model_path = './GoogleNews-vectors-negative300.bin'
@@ -47,6 +55,11 @@ pre_embd = load_embd_weights(word2vec, vocab_size, args.embd_size, w2i)
 args.pre_embd = pre_embd
 
 
+def save_checkpoint(state, filename):
+    print('save model!', filename)
+    torch.save(state, filename)
+
+
 def loss_fn(pos_sim, neg_sim):
     loss = args.margin - pos_sim + neg_sim
     if loss.data[0] < 0:
@@ -54,9 +67,9 @@ def loss_fn(pos_sim, neg_sim):
     return loss
 
 
-def train(model, data, optimizer, n_epochs=4, batch_size=64):
-    model.train()
+def train(model, data, test_data, optimizer, n_epochs=4, batch_size=256):
     for epoch in range(n_epochs):
+        model.train()
         print('epoch', epoch)
         random.shuffle(data) # TODO use idxies
         losses = []
@@ -76,13 +89,20 @@ def train(model, data, optimizer, n_epochs=4, batch_size=64):
                     break
 
             if len(losses) == batch_size or i == len(data) - 1:
-                # print('batch_size=', len(losses))
                 loss = torch.mean(torch.stack(losses, 0).squeeze(), 0)
                 print(loss.data[0])
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
                 losses = []
+
+        filename = '{}/Epoch-{}.model'.format('./checkpoints', epoch)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'optimizer' : optimizer.state_dict(),
+        }, filename=filename)
+        test(model, test_data)
 
 
 def test(model, data):
@@ -111,7 +131,7 @@ def test(model, data):
         else:
             print('wrong')
         total += 1
-    print('Acc:', 100*acc/total)
+    print('Test Acc:', 100*acc/total, '%')
 
 
 model = QA_LSTM(args)
@@ -120,5 +140,15 @@ if torch.cuda.is_available():
 # optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, model.parameters()), lr=0.01)
 optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
 
-train(model, train_data, optimizer)
-test(model, test_data)
+if os.path.isfile(args.resume):
+    print("=> loading checkpoint '{}'".format(args.resume))
+    checkpoint = torch.load(args.resume)
+    args.start_epoch = checkpoint['epoch']
+    # best_prec1 = checkpoint['best_prec1']
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer']) # TODO ?
+    print("=> loaded checkpoint '{}' (epoch {})".format(args.resume, checkpoint['epoch']))
+else:
+    print("=> no checkpoint found at '{}'".format(args.resume))
+train(model, train_data, test_data, optimizer)
+# test(model, test_data)
